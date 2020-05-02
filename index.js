5d2ed0e17a8d951659ab4233e1363feb129a40d8
Giulio Ambrogi <giulio.ambrogi@gmail.com>
2020-05-02 09:43:40 +0100

    CLEANUP

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var FormManager = function () {
   var form = document.querySelector('form#add-todo');
 
@@ -5,29 +6,32 @@ var FormManager = function () {
 
   var itemsList = document.querySelector('ul')
 
+  var createDeleteButton = function (itemIndex) {
+    var deleteBtn = document.createElement('button')
+    deleteBtn.addEventListener('click', (e) => deleteItem(itemIndex))
+    deleteBtn.className = "btn btn-danger"
+    deleteBtn.innerText = 'Delete'
+    deleteBtn.style.float = 'right'
+    return deleteBtn;
+  }
+
   var refreshItemsList = function () {
     itemsList.innerHTML = null;
     items.forEach(function (item, index) {
       var span = document.createElement('span')
       span.innerText = item;
 
-      var deleteBtn = document.createElement('button')
-      deleteBtn.addEventListener('click', deleteItem)
-      deleteBtn.className = "btn btn-danger"
-      deleteBtn.style.float = 'right'
-      deleteBtn.innerText = 'Delete'
+      var deleteBtn = createDeleteButton(index)
 
       var el = document.createElement('li')
       el.appendChild(span)
       el.appendChild(deleteBtn)
       el.className = "list-group-item"
-      el.setAttribute('data-index', index)
       itemsList.appendChild(el)
     })
   }
 
-  var deleteItem = function (e) {
-    var index = e.target.getAttribute('data-index')
+  var deleteItem = function (index) {
     items = items.slice(0, index).concat(items.slice(index + 1, items.length))
     refreshItemsList();
   }
