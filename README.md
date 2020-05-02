0326172f845a033392c204d5a767d6a08bf06649
Giulio Ambrogi <giulio.ambrogi@gmail.com>
2020-05-02 10:01:17 +0100

    REFACTOR

--- README.md
+++ README.md
@@ -27,3 +27,18 @@ or if you want to play with the Cypress UI:
 ```
 yarn test:interactive
 ```
+
+
+# Notes
+
+In some areas the code is intentionally verbose to show how we can manipulate various attributes of a DOM element, such as:
+
+* Enforcing some style by setting the [`style` property](https://developer.mozilla.org/en-US/docs/Web/API/ElementCSSInlineStyle/style). Which in most cases can be done in CSS but in some edge cases you may want to use JS.
+* Setting the `className` of an element
+* Using `appendChild` to add an element inside another element
+
+# Resources
+
+Some useful resources:
+- [MDN Docs](https://developer.mozilla.org/it/docs/Web/API/Document_Object_Model)
+- [A crash course on youtube](https://www.youtube.com/watch?v=0ik6X4DJKCc)
--- index.js
+++ index.js
@@ -6,42 +6,11 @@ var FormManager = function () {
 
   var itemsList = document.querySelector('ul')
 
-  var createDeleteButton = function (itemIndex) {
-    var deleteBtn = document.createElement('button')
-    deleteBtn.addEventListener('click', (e) => deleteItem(itemIndex))
-    deleteBtn.className = "btn btn-danger"
-    deleteBtn.innerText = 'Delete'
-    deleteBtn.style.float = 'right'
-    return deleteBtn;
-  }
-
-  var refreshItemsList = function () {
-    itemsList.innerHTML = null;
-    items.forEach(function (item, index) {
-      var span = document.createElement('span')
-      span.innerText = item;
-
-      var deleteBtn = createDeleteButton(index)
-
-      var el = document.createElement('li')
-      el.appendChild(span)
-      el.appendChild(deleteBtn)
-      el.className = "list-group-item"
-      itemsList.appendChild(el)
-    })
-  }
-
-  var deleteItem = function (index) {
-    items = items.slice(0, index).concat(items.slice(index + 1, items.length))
-    refreshItemsList();
-  }
-
-  var addTodoItem = function (value) {
-    items.push(value)
-    refreshItemsList()
-  }
-
-  var onSubmit = function (e) {
+  /**
+   * Handle the submit and adds the item if the input field is non-empty
+   * @param {DOMEvent} e 
+   */
+  function onSubmit(e) {
     e.preventDefault();
     var todoItemValue = e.target.querySelector('#item').value
     if (todoItemValue != null && todoItemValue.length > 0) {
@@ -50,17 +19,60 @@ var FormManager = function () {
     console.log(e)
   }
 
-  form.addEventListener('submit', onSubmit)
-  var inputField = form.querySelector('#item')
+  /**
+   * Creates the DOM Element of the Delete button
+   * @param {Number} itemIndex The index of the item used when deleting it 
+   */
+  function createDeleteButton(itemIndex) {
+    var deleteBtn = document.createElement('button')
+    deleteBtn.addEventListener('click', (e) => deleteItem(itemIndex))
+    deleteBtn.className = "btn btn-danger"
+    deleteBtn.innerText = 'Delete'
+    deleteBtn.style.float = 'right'
+    return deleteBtn;
+  }
 
-  var originalBorder = inputField.style.border
-  inputField.addEventListener('mouseenter', e => {
-    e.target.style.border = '4px solid black'
-  })
-  inputField.addEventListener('mouseleave', e => {
-    e.target.style.border = originalBorder
-  })
-  return;
+  /**
+   * Updates the list of items displayed on the page.
+   */
+  function refreshItemsList() {
+    itemsList.innerHTML = null;
+    items.forEach(function (item, index) {
+      var text = document.createElement('span')
+      text.innerText = item;
+
+      var el = document.createElement('li')
+      el.appendChild(text)
+      el.appendChild(createDeleteButton(index))
+      el.className = "list-group-item"
+
+      itemsList.appendChild(el)
+    })
+  }
+
+  function deleteItem(index) {
+    items = [...items.slice(0, index), ...items.slice(index + 1, items.length)]
+    refreshItemsList();
+  }
+
+  function addTodoItem(value) {
+    items.push(value)
+    refreshItemsList()
+  }
+
+  function setupInputEffects() {
+    var inputField = form.querySelector('#item')
+    var originalBorder = inputField.style.border
+    inputField.addEventListener('mouseenter', e => {
+      e.target.style.border = '4px solid black'
+    })
+    inputField.addEventListener('mouseleave', e => {
+      e.target.style.border = originalBorder
+    })
+  }
+
+  form.addEventListener('submit', onSubmit)
+  setupInputEffects()
 }
 
 document.addEventListener("DOMContentLoaded", function () {
